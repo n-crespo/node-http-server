7445668d8651fcf5d3e231e05fdb1f5bfebf1cb5
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-10-20 23:12:37 -0700

    chore: init

---
+++ index.js
@@ -0,0 +1,19 @@
+import http from "http";
+const PORT = 8000;
+
+// create a server and write hello world
+const server = http.createServer((req, res) => {
+  // res.setHeader("Content-Type", "text/html");
+  // res.statusCode = 404;
+  res.writeHead(500, { "content-type": "application/json" });
+  res.end(
+    JSON.stringify({
+      message: "hello world",
+    }),
+  );
+});
+
+// listen!
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
